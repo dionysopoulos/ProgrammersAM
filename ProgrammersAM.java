2218a118aca5f3b28731e4a189846a33c54a9fbf
GiannisNikolopoulos05 <t8230246@aueb.gr>
2024-11-16 19:38:27 +0200

    t8230246 Register

--- ProgrammersAM.java
+++ ProgrammersAM.java
@@ -3,5 +3,6 @@ public class ProgrammersAM {
         System.out.println("t8230033");
         System.out.println("t8220228");
         System.out.println("t8230047");
+        System.out.println("t8230246");
     }
 }
