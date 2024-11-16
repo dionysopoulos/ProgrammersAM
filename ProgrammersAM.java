c2551627188fc50486d5df4553a524538f37b721
Emmanouil Karakatsanis <t8230047@aueb.gr>
2024-11-16 19:33:06 +0200

    8230047 register

--- ProgrammersAM.java
+++ ProgrammersAM.java
@@ -2,5 +2,6 @@ public class ProgrammersAM {
     public static void main (String[] args) {
         System.out.println("t8230033");
         System.out.println("t8220228");
+        System.out.println("t8230047");
     }
 }
