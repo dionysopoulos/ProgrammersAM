3ceb3ca1bb397211036059294f93a356724a2741
DIMITRIOS KATSOULIS <t8230056@aueb.gr>
2024-11-16 19:50:57 +0200

    8230056 Register

--- ProgrammersAM.java
+++ ProgrammersAM.java
@@ -4,5 +4,6 @@ public class ProgrammersAM {
         System.out.println("t8220228");
         System.out.println("t8230047");
         System.out.println("t8230246");
+        System.out.println("t8230056");
     }
 }
